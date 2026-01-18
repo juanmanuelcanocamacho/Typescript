0c5e4346de5d858ddb0f13dd3bc1b19ac665b629
Juan Manuel Cano <juanmanuel.cano@citters.com>
2026-01-18 10:23:17 +0100

    18/01/2026

---
+++ Objetos/objects.ts
@@ -0,0 +1,28 @@
+(()=> {
+
+/*     let hero = {
+        name: 'Daniel',
+        age: 6,
+        power: ['Fuerza', 'Invisible'],
+        getName ():string {
+            return this.name
+        }
+
+    }
+
+    console.log(hero.name);
+
+    console.log(hero.getName()) */
+
+
+    
+
+
+
+
+
+
+
+
+
+})()
